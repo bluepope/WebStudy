f1b88f55005066462b911993e38def27c0d4942e
안현모 <blue_pope@hotmail.com>
2020-03-05 16:09:01 +0900

    이것저것

---
+++ BluePope.WebApp/Pages/Level2/Privacy.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BluePope.WebApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(string x)
+        {
+            var y = Request.Query["y"];
+
+            ViewData["x"] = x;
+            ViewData["y"] = y;
+        }
+    }
+}
